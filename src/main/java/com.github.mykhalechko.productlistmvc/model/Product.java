0abd354632314c2c2308e69cf1735e00d00db7cb
0vector0 <0vector0@gmail.com>
2016-09-20 21:01:38 +0300

    add product model

---
+++ src/main/java/com.github.mykhalechko.productlistmvc/model/Product.java
@@ -0,0 +1,27 @@
+package com.github.mykhalechko.productlistmvc.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 3749019693491946812L;
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+
+}
