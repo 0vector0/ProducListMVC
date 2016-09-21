d4e8c64a449b1b851a4bbaac0e2546e73166b1d6
0vector0 <0vector0@gmail.com>
2016-09-21 21:45:48 +0300

    add Controller and fix Service from Product

--- src/main/java/com/github/mykhalechko/productlist/service/ProductServiceImpl.java
+++ src/main/java/com/github/mykhalechko/productlist/service/ProductServiceImpl.java
@@ -2,14 +2,15 @@ package com.github.mykhalechko.productlist.service;
 
 import com.github.mykhalechko.productlist.dao.ProductDao;
 import com.github.mykhalechko.productlist.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
+    @Autowired
     private ProductDao productDao;
 
     public void setProductDao(ProductDao productDao) {
@@ -17,31 +18,26 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    @Transactional
     public void addProduct(Product product) {
         this.productDao.addProduct(product);
     }
 
     @Override
-    @Transactional
     public void updateProduct(Product product) {
         this.productDao.updateProduct(product);
     }
 
     @Override
-    @Transactional
     public void removeProduct(int id) {
         this.productDao.removeProduct(id);
     }
 
     @Override
-    @Transactional
     public Product getProductById(int id) {
         return this.productDao.getProductById(id);
     }
 
     @Override
-    @Transactional
     public List<Product> listProducts() {
         return this.productDao.listProducts();
     }
