c0d7205840afabb1743aba43f6f42575d7ccd95f
0vector0 <0vector0@gmail.com>
2016-12-22 00:26:26 +0200

    clean code

--- src/main/java/com/github/mykhalechko/productlist/controller/IndexController.java
+++ src/main/java/com/github/mykhalechko/productlist/controller/IndexController.java
@@ -2,13 +2,12 @@ package com.github.mykhalechko.productlist.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping(value = "/", name = "getIndex", method = RequestMethod.GET)
+    @RequestMapping(value = "/")
     public ModelAndView getIndex() {
         return new ModelAndView("index");
     }
