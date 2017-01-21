5c06ad642117bb9bd7c458f49fc566afbc5d6e76
0vector0 <0vector0@gmail.com>
2017-01-21 22:35:56 +0200

    use thymeleaf layout

--- src/main/java/com/github/mykhalechko/productlist/SecurityConfig.java
+++ src/main/java/com/github/mykhalechko/productlist/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/readme.txt", "/css/*", "/registration", "/index", "/", "/content").permitAll()
+                .antMatchers("/readme.txt", "/css/*", "/registration", "/index", "/").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
