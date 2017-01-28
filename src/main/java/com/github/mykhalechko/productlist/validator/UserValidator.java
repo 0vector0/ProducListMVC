2240a89924b42306c792208a7031079f568d4ed9
0vector0 <0vector0@gmail.com>
2017-01-29 01:58:09 +0200

    errors in registration

--- src/main/java/com/github/mykhalechko/productlist/validator/UserValidator.java
+++ src/main/java/com/github/mykhalechko/productlist/validator/UserValidator.java
@@ -3,6 +3,7 @@ package com.github.mykhalechko.productlist.validator;
 import com.github.mykhalechko.productlist.model.User;
 import com.github.mykhalechko.productlist.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
@@ -14,6 +15,11 @@ public class UserValidator implements Validator {
     @Autowired
     private UserService userService;
 
+
+    @Value("${size.userForm.username}")
+    private String sizeUsername;
+    // TODO: 29.01.2017 use another filed from validation.properties
+
     @Override
     public boolean supports(Class<?> aClass) {
         return User.class.equals(aClass);
@@ -24,19 +30,23 @@ public class UserValidator implements Validator {
         User user = (User) o;
 
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "NotEmpty");
-        if (user.getUsername().length() < 1 || user.getUsername().length() > 32) {
-            errors.rejectValue("username", "Size.userForm.username");
+        if (user.getUsername().length() < 2 || user.getUsername().length() > 32) {
+            System.out.println(sizeUsername);
+            errors.rejectValue("username", sizeUsername);
         }
         if (userService.findByUsername(user.getUsername()) != null) {
+            System.out.println("Duplicate.userForm.username");
             errors.rejectValue("username", "Duplicate.userForm.username");
         }
 
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty");
         if (user.getPassword().length() < 2 || user.getPassword().length() > 32) {
+            System.out.println("Size.userForm.password");
             errors.rejectValue("password", "Size.userForm.password");
         }
 
         if (!user.getPasswordConfirm().equals(user.getPassword())) {
+            System.out.println("Diff.userForm.passwordConfirm");
             errors.rejectValue("passwordConfirm", "Diff.userForm.passwordConfirm");
         }
     }
