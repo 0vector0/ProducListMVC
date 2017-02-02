4e536de727f2a6fa7b322ec59766081f794f60a9
0vector0 <0vector0@gmail.com>
2017-02-03 00:05:59 +0200

    fix validator for registration page

--- src/main/java/com/github/mykhalechko/productlist/validator/UserValidator.java
+++ src/main/java/com/github/mykhalechko/productlist/validator/UserValidator.java
@@ -3,7 +3,6 @@ package com.github.mykhalechko.productlist.validator;
 import com.github.mykhalechko.productlist.entity.User;
 import com.github.mykhalechko.productlist.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
@@ -15,19 +14,6 @@ public class UserValidator implements Validator {
     @Autowired
     private UserService userService;
 
-    @Value("${userForm.username}")
-    private String username;
-
-    @Value("${userForm.email}")
-    private String email;
-
-    @Value("${userForm.password}")
-    private String password;
-
-    @Value("${userForm.passwordConfirm}")
-    private String passwordConfirm;
-
-
     @Override
     public boolean supports(Class<?> aClass) {
         return User.class.equals(aClass);
@@ -38,29 +24,25 @@ public class UserValidator implements Validator {
         User user = (User) o;
 
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "NotEmpty");
+
         if (user.getUsername().length() < 2 || user.getUsername().length() > 32) {
-            System.out.println(username);
-            errors.rejectValue("username", username);
+            errors.rejectValue("username", "size.username");
         }
         if (userService.findByUsername(user.getUsername()) != null) {
-            System.out.println(username);
-            errors.rejectValue("username", username);
+            errors.rejectValue("username", "already.has.username");
         }
 
         if (userService.findByEmail(user.getEmail()) != null) {
-            System.out.println(email);
-            errors.rejectValue("email", email);
+            errors.rejectValue("email", "already.has.email");
         }
 
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty");
         if (user.getPassword().length() < 2 || user.getPassword().length() > 32) {
-            System.out.println(password);
-            errors.rejectValue("password", password);
+            errors.rejectValue("password", "size.password");
         }
 
         if (!user.getPasswordConfirm().equals(user.getPassword())) {
-            System.out.println(passwordConfirm);
-            errors.rejectValue("passwordConfirm", passwordConfirm);
+            errors.rejectValue("passwordConfirm", "confirm.password");
         }
     }
 }
