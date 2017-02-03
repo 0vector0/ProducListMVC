21ca176330c4d517de233588eba0a49391a222ab
0vector0 <0vector0@gmail.com>
2017-02-03 03:13:27 +0200

    add sql scripts

---
+++ src/main/resources/sql/productlist_public_roles.sql
@@ -0,0 +1,2 @@
+INSERT INTO roles (id, name) VALUES (1, 'ROLE_ADMIN');
+INSERT INTO roles (id, name) VALUES (2, 'ROLE_USER');
---
+++ src/main/resources/sql/productlist_public_users.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.users (id, email, password, username) VALUES (32, 'user@gmail.com', '$2a$10$kC6c0n25ldrmMijNworJqOeF2fMOoKkojHY01eqWBE7.AK89tvWAO', 'user');
+INSERT INTO public.users (id, email, password, username) VALUES (35, 'test@gmail.com', '$2a$10$3WzITXLLTMnXL0iAihXiZelFN46ypHZpXAWetYmfAgIXBVsI2pH1K', 'test');
+INSERT INTO public.users (id, email, password, username) VALUES (41, 'admin@gmail.com', '$2a$10$GsWe.5ij90t5FsWcXudBTum.zhEd2xKW0GmR6XZW/Hqh9GBkt.ypK', 'admin');
